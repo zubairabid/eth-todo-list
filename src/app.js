65a27280477ba5aebc67ff0afb17fce763091621
zubairabid <zubairabid1999+github@gmail.com>
2019-09-16 03:03:14 +0530

    Create task from frontend, tests

--- src/app.js
+++ src/app.js
@@ -68,6 +68,13 @@ App = {
         App.setLoading(false)
     },
 
+    createTask: async () => {
+        App.setLoading(true)
+        const content = $('#newTask').val()
+        await App.todoList.createTask(content)
+        window.location.reload()
+    },
+
     setLoading: (boolean) => {
         App.loading = boolean
         const loader = $("#loader")
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -24,4 +24,16 @@ contract('TodoList', (accounts) => {
         
 
     })
+
+
+    it('created tasks', async () => {
+        const result = await this.todoList.createTask('A new task')
+        const taskCount = await this.todoList.taskCount()
+        assert.equal(taskCount, 2)
+        const event = result.logs[0].args
+        assert.equal(event.id.toNumber(), 2)
+        assert.equal(event.content, 'A new task')
+        assert.equal(event.completed, false)
+
+    })
 })
