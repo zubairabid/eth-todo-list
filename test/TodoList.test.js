c51bb62b6b977870fb7e7a9aa526eb21537d38de
zubairabid <zubairabid1999+github@gmail.com>
2019-09-16 02:20:04 +0530

    Listing and testing

---
+++ test/TodoList.test.js
@@ -0,0 +1,27 @@
+const TodoList = artifacts.require('TodoList.sol')
+
+contract('TodoList', (accounts) => {
+    before(async () => {
+        this.todoList = await TodoList.deployed()
+    })
+
+    it('deployed successfully', async () => {
+        const address = await this.todoList.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+
+    it('list tasks', async () => {
+        const taskCount = await this.todoList.taskCount()
+        const task = await this.todoList.tasks(taskCount)
+
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.content, 'Do the actual assignment')
+        assert.equal(task.completed, false)
+        assert.equal(taskCount, 1)
+        
+
+    })
+})
